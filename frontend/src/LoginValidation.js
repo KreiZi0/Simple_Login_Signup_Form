d1e198320ad7059c4d96585a08a77329b67eec41
KreiZi0 <steven.mirontsuk1@gmail.com>
2023-11-15 16:23:10 +0200

    LogIn Validation

---
+++ frontend/src/LoginValidation.js
@@ -0,0 +1,29 @@
+function Validation(values) {
+    let error = {}
+    const email_pattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    const password_pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+
+    if(values.email === "") {
+        error.email = "Nimi ei tohi olla tühi"
+    }
+    else if(!email_pattern.test(values.email)) {
+        error.email = "Email ei sobi"
+    }else {
+        error.email = ""
+    }
+
+    if(values.password === "") {
+        error.password = "Salasõna ei tohi olla tühi"
+    }
+    else if(!password_pattern.test(values.password)) {
+        error.password = "Salasõna ei sobi. Peab sisaldama minimaalselt kaheksa märki - vähemalt üks täht ja üks number"
+    }else {
+        error.password = ""
+    }
+    return error;
+
+}
+
+export default Validation;
+
+
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import Validation from './LoginValidation'
 
 export default function Login() {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({})
+    
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(values))
+
+    }
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-secondary vh-100 '>
         <div className='bg-white p-3 rounded w-25'>
-            <form action="">
+            <h2>Logi sisse</h2>
+            <form action="" onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor="email"><strong>Email:</strong></label>
-                    <input type="email" placeholder='Sisesta email' className='form-control rounded-0'></input>
+                    <input type="email" placeholder='Sisesta email' name='email'
+                    onChange={handleInput} className='form-control rounded-0'></input>
+                    {errors.email && <span className='text-danger'>{errors.email}</span>}
                 </div>
                 <div className='mb-3'>
                     <label htmlFor="password"><strong>Salasõna:</strong></label>
-                    <input type="password" placeholder='Sisesta salasõna' className='form-control rounded-0' ></input>
+                    <input type="password" placeholder='Sisesta salasõna' name='password'
+                    onChange={handleInput} className='form-control rounded-0' ></input>
+                    {errors.password && <span className='text-danger'>{errors.password}</span>}
                 </div>
-                <button className='btn align-items-center btn-primary w-100 rounded-0'><strong>Logi sisse</strong></button>
+                <button type='submit 'className='btn align-items-center btn-primary w-100 rounded-0'><strong>Logi sisse</strong></button>
                 <p></p>
                 <Link to="/signup" className='btn btn-light w-100 bg-light rounded-0'>Loo konto</Link>
             </form>
